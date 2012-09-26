3713cb95b96ed1d0210fabeed8ee8ba0f9e20a81
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2012-09-26 19:22:40 +0200

    Added minified build

--- build.rb
+++ build.rb
@@ -1,5 +1,38 @@
 #!/usr/bin/env ruby
 
+require 'net/http'
+require 'uri'
+require 'optparse'
+
+options = {}
+
+OptionParser.new { |opts|
+	opts.banner = 'Usage: build.rb [options]'
+
+	opts.on('-m', '--minify', 'Minify souce using the Google Closure Compiler') do 
+		options[:minify] = true
+	end
+}.parse!
+
+GOOGLE_CLOSURE_COMPILER = URI.parse('http://closure-compiler.appspot.com/compile')
+
+def minify(src)
+	Net::HTTP.start(GOOGLE_CLOSURE_COMPILER.host, GOOGLE_CLOSURE_COMPILER.port) do |http|
+		request = Net::HTTP::Post.new(GOOGLE_CLOSURE_COMPILER.request_uri)
+		request.set_form_data({
+			:js_code => src,
+			:compilation_level => 'SIMPLE_OPTIMIZATIONS',
+			:output_format => 'text',
+			:output_info => 'compiled_code'
+		})
+
+		response = http.request(request)
+
+		response.value
+		response.body
+	end
+end
+
 dir = File.absolute_path(File.dirname(__FILE__))
 template = "// File -- %s\n%s"
 
@@ -9,10 +42,13 @@ Dir.glob(File.join(dir, 'lib', '*')).push(File.join(dir, 'index.js')).each do |p
 	path = File.absolute_path(path, dir)
 
 	File.open(path, 'r') do |f|
-		out << template % [path.gsub(dir, '/').gsub(/^\/*/, './'), f.read()]
+		out << template % [path.gsub(dir, '/').gsub(/^\/*/, './'), f.read]
 	end
 end
 
+out = out.join("\n")
+out = options[:minify] ? minify(out) : out 
+
 File.open(File.join(dir, 'jquery-observe.js'), 'w') do |f|
-	f.write(out.join("\n"))
+	f.write(out)
 end
